0247659e7d36fcf8a7caab05a6d61a8647531dc7
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-24 23:32:56 +0530

    App jsx file updated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react"
 import { TodoProvider } from "./contexts"
+import TodoForm from "./components/TodoForm"
+import TodoItem from "./components/TodoItem"
 
 function App() {
-  const [Todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([])
 
   const addTodo = (todo) =>{
     setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
@@ -43,9 +45,17 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                            <div key={todo.id}
+                            className="w-full">
+                              <TodoItem todo={todo} />
+                            </div>
+                          ))
+                        }
                     </div>
                 </div>
             </div>
